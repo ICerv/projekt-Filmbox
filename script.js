bd42883b5b2c0daf02279b566b8f1fea795ff31b
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-04-12 18:19:57 +0200

    Hamburger menu + bonus

--- script.js
+++ script.js
@@ -106,3 +106,18 @@ const filmy = [
 		premiera: '2022-12-24',
 	},
 ]
+
+/*********MENU*********/
+
+const menuTlacitkoElm = document.querySelector('#menu-tlacitko');
+const menuPolozkyElm = document.querySelector('#menu-polozky');
+
+menuTlacitkoElm.addEventListener('click', () => {
+	menuPolozkyElm.classList.toggle('show')
+	if (menuPolozkyElm.classList.contains('show')) {
+		menuTlacitkoElm.innerHTML = '<i class="fas fa-xmark"></i>'
+	} else {
+		menuTlacitkoElm.innerHTML = '<i class="fas fa-bars"></i>'
+	}
+});
+

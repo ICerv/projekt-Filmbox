396b0a79f66c7b79de6d4f9c8de4e898f03a32e5
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-04-12 19:21:26 +0200

    Seznam filmu

--- script.js
+++ script.js
@@ -105,6 +105,7 @@ const filmy = [
 			'Na zámek v podhůří Krkonoš přijíždí jeho nový majitel Štěpán se svojí snoubenkou, krásnou komtesou Blankou, a mladším bratrem Adamem. Cestou kočár nešťastně srazí kolemjdoucí dívku, Adam jí pomůže a ona se do něj zamiluje. Na zámku Adam objeví starou vlašskou knihu, která by měla obsahovat cestu k pokladům. Tajemné značky vlašské knihy však nedokáže vyluštit ani národopisec Jiráček, který v kraji sbírá pověsti a nevychází z údivu nad tím, že zdejší lidé stále věří v Krakonoše. Na zámku se objeví záhadný cizinec a nabídne Štěpánovi, že jej k pokladu za určitých podmínek dovede. Výprava do hor může začít. Naplní se Liduščina láska k Adamovi? Jakou záhadu skrývá starý obraz na zámku Hůrka a co strašlivého se v horách kdysi odehrálo? A kdo je vlastně Krakonoš a jaké je jeho největší tajemství? (csfd.cz, Česká televize)',
 		premiera: '2022-12-24',
 	},
+
 ]
 
 /*********MENU*********/
@@ -120,4 +121,29 @@ menuTlacitkoElm.addEventListener('click', () => {
 		menuTlacitkoElm.innerHTML = '<i class="fas fa-bars"></i>'
 	}
 });
+/*********SEZNAM FILMU*********/
+const seznamFilmuElm = document.querySelector('#seznam-filmu');
+if (seznamFilmuElm) {
+	seznamFilmuElm.innerHTML = '';
+	filmy.forEach((film) => {
+		seznamFilmuElm.innerHTML += `
+<div class="col">
+  <div class="card">
+    <img
+      src="${film.plakat.url}"
+      width="${film.plakat.width}"
+      height="${film.plakat.height}"
+      class="card-img-top"
+      alt="plakát"
+    />
+    <div class="card-body">
+      <h5 class="card-title">${film.nazev}</h5>
+      <p class="card-text">${film.ochutnavka}</p>
+      <a href="film.html #${film.id}" class="btn btn-primary">Přehrát</a>
+    </div>
+  </div>
+</div>
+`
+	})
+}
 
